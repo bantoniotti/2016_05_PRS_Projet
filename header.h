7450873415b6065db88195d97f0128b89204fd2b
bantoniotti <bantoniotti@users.noreply.github.com>
2016-04-29 09:09:04 +0200

    Create header.h

---
+++ header.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <sys/select.h>
+#include <netinet/in.h>
+#include <errno.h>
+
